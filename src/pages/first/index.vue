cacf1b2667e70f8d1163257662bb3a88317f33ab
502 <270081484@qq.com>
2019-07-03 15:10:20 +0800

    Initial Commit

---
+++ src/pages/first/index.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="container">
+    <div class="first_box">
+      <div class="litter_bule"></div>
+      <p class="first_content">第一步，请根据角色面板填写以下属性</p>
+    </div>
+    <div class="pannle">
+      <div class="enter">
+        <p class="enter_title">等级:</p>
+        <input type="number" class="enter_input" placeholder="请输入数值" />
+      </div>
+      <div class="enter">
+        <p class="enter_title">神秘力量:</p>
+        <input type="number" class="enter_input" placeholder="请输入数值" />
+      </div>
+      <div class="enter">
+        <p class="enter_title">暴击：</p>
+        <input type="number" class="enter_input" placeholder="请输入数值" />
+      </div>
+      <div class="enter">
+        <p class="enter_title">暴击伤害：</p>
+        <input type="number" class="enter_input" placeholder="请输入数值" />
+      </div>
+      <div class="enter">
+        <p class="enter_title">冒险岛勇士：</p>
+        <input type="number" class="enter_input" value="15" placeholder="请输入数值" />
+      </div>
+      <div class="enter">
+        <p class="enter_title">超级主属性：</p>
+        <input type="number" class="enter_input" placeholder="请输入数值" />
+      </div>
+      <div class="enter">
+        <p class="enter_title">联盟系统提升的主属性 1 (攻击队占领效果提升的主属性):</p>
+        <input type="number" class="enter_input_long" placeholder="请输入数值" />
+      </div>
+      <div class="enter">
+        <p class="enter_title">联盟系统提升的主属性 2 (攻击队占领效果提升的主属性）:</p>
+        <input type="number" class="enter_input_long" placeholder="请输入数值" />
+      </div>
+    </div>
+    <img src="/static/images/next.png" class="next" background-size="cover" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+  
+  },
+
+  methods: {
+    clickHandle() {
+      this.msg = "Clicked!!!!!!";
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+.container {
+  background: rgba(255, 255, 255, 1);
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+.first_box {
+  margin-left: 5rpx;
+  margin-top: 26rpx;
+  display: flex;
+  flex-direction: row;
+
+  flex-wrap: nowrap;
+}
+.litter_bule {
+  width: 8px;
+  height: 20px;
+  background: rgba(102, 153, 255, 1);
+  border-radius: 4px;
+  margin-right: 15rpx;
+}
+.first_content {
+  height: 40rpx;
+  font-size: 28rpx;
+  font-family: PingFang-SC-Medium;
+  font-weight: 500;
+  color: rgba(51, 51, 51, 1);
+  border-bottom: 1px solid #eaeaea;
+}
+.next {
+  width: 90%;
+  margin-top: 5rpx;
+  height: 128rpx;
+  border-radius: 10%;
+}
+.pannle {
+  width: 750rpx;
+  height: 1100rpx;
+  background: rgba(255, 255, 255, 1);
+  padding-left: 30rpx;
+}
+.enter {
+  margin-top: 45rpx;
+  padding-block-end: 10rpx;
+  border-bottom: 1px solid #eaeaea;
+  display: flex;
+  flex-direction: row;
+    flex-wrap: wrap;
+}
+.enter_title {
+  height: 50rpx;
+  font-size: 40rpx;
+  font-family: PingFang-SC-Medium;
+  font-weight: 500;
+  color: rgba(153, 153, 153, 1);
+}
+.enter_input {
+  height: 50rpx;
+  font-size: 30rpx;
+  font-family: PingFang-SC-Medium;
+  font-weight: 500;
+  color: rgba(153, 153, 153, 1);
+  justify-content: flex-end;
+}
+.enter_input_long{
+    height: 50rpx;
+    margin-top: 70rpx;
+  font-size: 30rpx;
+  font-family: PingFang-SC-Medium;
+  font-weight: 500;
+  color: rgba(153, 153, 153, 1);
+  justify-content: flex-end;
+}
+
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="container">
+    <p class="warring">注意事项</p>
+    <div class="introduction">
+      <p class="warring-content">
+        1、目前不支持恶魔复仇者、尖兵、剑豪等攻击力计算
+        比较特殊的职业
+      </p>
+      <p class="warring-content">2、请将每个部位尽量填写完整</p>
+      <p class="warring-content">3、目前任为开发版本，开发进度1%</p>
+      <p class="warring-content">
+        4、基础思路学习冒险岛属性对应excel，当开发版本低
+        时思路相近
+      </p>
+      <p class="warring-content">5、仅发布在B站冒险岛交流群（957018793）内测</p>
+      <p class="warring-content">6、介于作者只是一个垃圾后端前段能力令人发指，有问题见谅一下</p>
+    </div>
+    <a href="../../pages/first/main" class="next_a">
+      <img src="/static/images/next.png" class="next" background-size="cover" />
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Hello w111"
+    };
+  },
+
+  methods: {
+    clickHandle() {
+      this.msg = "Clicked!!!!!!";
+    }
+  }
+};
+</script>
+
+<style scoped>
+.message {
+  color: red;
+  padding: 10rpx;
+  text-align: center;
+}
+.container {
+  background: rgba(255, 255, 255, 1);
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+.introduction {
+  margin-top: 10%;
+  width: 90%;
+  background: rgba(255, 255, 255, 1);
+  box-shadow: 0rpx 3rpx 44rpx 2rpx rgba(0, 0, 0, 0.1);
+  border-radius: 10rpx;
+}
+.warring {
+  /* width: 126rpx; */
+  height: 31rpx;
+  margin-top: 5%;
+  font-size: 60rpx;
+  font-family: PingFang-SC-Medium;
+  font-weight: 500;
+  color: rgba(51, 51, 51, 1);
+}
+.warring-content {
+  font-size: 26rpx;
+  font-family: PingFang-SC-Regular;
+  font-weight: 400;
+  color: rgba(102, 102, 102, 1);
+  line-height: 36rpx;
+  margin-top: 5%;
+  margin-right: 4%;
+  margin-left: 4%;
+  margin-bottom: 10%;
+}
+.next {
+  width: 712rpx;
+  height: 122rpx;
+}
+.next_a {
+    width: 95%;
+    margin-top: 5rpx;
+ border-radius: 10%;
+}
+</style>
